8b2030ea7ffbe95395fadc6efb23cd75a9e816e9
andrewgorman <andrew@gandrewgorman.dev>
2021-02-05 15:19:31 -0600

    Organizes directory

--- home/models.py
+++ home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 
-class HomePage(Page):
-    pass
+class RecipePage(Page):
+    description = RichTextField(blank=True, max_length=200, features=['bold', 'italic', 'link'])
+    ingredients = RichTextField(features=['bold', 'italic', 'link', 'ul'])
+    body = RichTextField(features=['h2', 'h3', 'h4', 'bold', 'italic', 'link', 'ol'])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
